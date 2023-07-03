11bdd26327092896ac5d6221dc2eeb6a40a706a6
Andres Ramirez <andres.ramirez9912@gmail.com>
2023-07-02 20:23:15 -0500

    Print the Height on the screen

--- esp32Code/bmp180.ino
+++ esp32Code/bmp180.ino
@@ -1,3 +1,19 @@
-float getHeight(Adafruit_BMP085* bmp){
+float getAltitude(Adafruit_BMP085* bmp){
+  // const int numberValues =20;
+  // float average=0;
+
+  // for(short i=0; i<numberValues; i++) {
+  //   average = bmp->readAltitude(101300) + average; // Read the altitude with the standar sea level pressure
+  //   delay(30); // Delay between measurement
+  // }
+  // return  average/numberValues;
   return bmp->readAltitude(101300); // Read the altitude with the standar sea level pressure
+}
+
+float getPressure(Adafruit_BMP085* bmp){
+  return bmp->readPressure();
+}
+
+float getHeightByReference(Adafruit_BMP085* bmp, float pressureReference){
+  return bmp->readAltitude(pressureReference);
 }
--- esp32Code/heightScreen.ino
+++ esp32Code/heightScreen.ino
@@ -1,17 +1,19 @@
 void showHeightScreen(Adafruit_SSD1306* display, Adafruit_BMP085* bmp){
-  // Variables 
-  float height = 0;  
+
+  // Get the reference pressure
+  float referenceHeight = getAltitude(bmp); 
 
   while(true){ // Loop to measure
     // Clean the display from the main Menu
     display->clearDisplay();
 
-    // Get the Height 
-    height = getHeight(bmp);
+    // Get the Height by the reference
+    float currentHeight = getAltitude(bmp); 
+    float height = currentHeight - referenceHeight;
 
     // Print the Height  
     printHeight(display, height);
     Serial.println(height);
-    delay(100);
+    delay(50);
   }
 }
